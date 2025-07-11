e7bf39162a4efed667f2a3a1aa1a9b696d1bb5a8
leolalicata <leo.lalicata@gmail.com>
2025-07-11 11:35:28 +0200

    simplex method implemented in an external function
    the code works fine with Bishop and Spencer, but not with Morgenstern & Price.
    double check needed.

--- src/limit-equilibrium/gridSimplexComputation.py
+++ src/limit-equilibrium/gridSimplexComputation.py
@@ -1,22 +1,34 @@
 import time
-from gridOfCircles import gridOfCircles
-from base_classes import UniformQuadrature,Options,np
+from gridOfCircles import gridOfCircles , computeEtaMinForSurface, gridComputation
+from circularSlipSurface import circularSlipSurface
+from base_classes import UniformQuadrature,Options,np, optimize
 
+def simplexComputation(method,ground_surface, bounding_box,soil_properties,soil_state,gridOptions,methodOptions,bounds):
+    def func(v):
+        x_in , x_out , eta = v[0] , v[1] , np.radians(v[2])
+        func.calls+=1
+        eta_min=computeEtaMinForSurface(ground_surface,bounding_box,x_in,x_out)
+        if eta < eta_min+1.e-3:
+            return np.nan
+        else:
+            geometry=circularSlipSurface.fromInOutAndEta(ground_surface,bounding_box,x_in,x_out,eta)
+            return method(geometry,soil_properties,soil_state,methodOptions).factor_of_safety
+    func.calls=0
+    [result,time_duration]=gridComputation(method, ground_surface,bounding_box,soil_properties,soil_state,gridOptions,methodOptions)
+    start_geo=result[0].inputs[0]
+    trial=[start_geo.landslide_interval[1],start_geo.landslide_interval[0],np.degrees(start_geo.eta)]
+    # probabilmente le opzioni del simplesso andrebbero definite da qualche parte
+    simplex_result = optimize.minimize(func, trial , 
+                          method='Nelder-Mead',
+                          bounds = bounds,
+                          options = {
+                            'disp'      : False,
+                            'xatol'     : 1e-3,
+                            'fatol'     : 1e-4,
+                            'maxiter'   : 100,
+                            'return_all': True
+                            }
+                          )
+    
+    return simplex_result,func.calls
 
-class GridOptions:
-    def __init__(self,in_interval,out_interval,min_eta_inc=np.radians(5),num_in_pts=10,num_out_pts=10):
-        self.in_interval=in_interval
-        self.out_interval=out_interval
-        self.min_eta_inc=min_eta_inc
-        self.num_in_pts=num_in_pts
-        self.num_out_pts=num_out_pts
-
-
-
-def gridComputation(method,ground_surface, bounding_box,soil_properties,soil_state,gridOptions,methodOptions):
-    geometries=gridOfCircles(ground_surface, bounding_box, gridOptions.in_interval,gridOptions.out_interval,gridOptions.min_eta_inc,gridOptions.num_in_pts,gridOptions.num_out_pts)
-    time_start = time.perf_counter()
-    result=[method(geometry,soil_properties,soil_state,methodOptions) for geometry in geometries]
-    time_duration = time.perf_counter()- time_start
-    result.sort(key=lambda x: x.factor_of_safety)
-    return result,time_duration
