d2291cff8a58c4cf02597d511b95cdfe8566a15a
Andrea Bressan <andrea.bressan@imati.cnr.it>
2025-11-11 16:35:30 +0100

    add some notes

--- src/test/test_bishop_1-left-up.py
+++ src/test/test_bishop_1-left-up.py
@@ -6,6 +6,13 @@ from base_classes import SoilProperties,SoilState,UniformQuadrature,Options,np,p
 from circularSlipSurface import circularSlipSurface
 from bishop import bishop
 
+# Safety Factor:  1.77447
+# varie combinazioni di segni
+# Safety Factor:  1.98608
+# Safety Factor: -1.67472
+# Safety Factor:  1.9006
+# Safety Factor:  2.58734
+
 geometry=circularSlipSurface.fromInOutAndEta(
     ground_surface=lambda x : 0*(x>3)+ (3-x)*(0<x)*(x<=3) + 3*(x<=0),
     bounding_box=np.array([[-5,10],[-5,9]]),
